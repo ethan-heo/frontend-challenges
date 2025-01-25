57bc954f9cf4859317d1f24b16f00f5cbba90205
ethan-heo <ethan.hb94@gmail.com>
2025-01-25 23:31:00 +0900

    fix: base 태그를 삭제하고 상대경로로 변경한다

--- apps/showcase/src/App.tsx
+++ apps/showcase/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="/projects/test" target="_blank">
+        <a href="projects/test" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
