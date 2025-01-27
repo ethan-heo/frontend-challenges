8828f9e4b01cd9e7082190dd7e4c86223cda9720
ethan-heo <ethan.hb94@gmail.com>
2025-01-27 15:42:08 +0900

    chore: 배포 환경 설정

---
+++ apps/conference-ticket-generator/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import moveDistFiles from "vite-plugin-move-dist-files";
+import path from "node:path";
+import pkg from "./package.json";
+
+export default defineConfig({
+  base: `/frontend-challenges/projects/${pkg.challenge.name}/`,
+  plugins: [
+    moveDistFiles({
+      distPath: path.resolve(__dirname, "dist"),
+      targetPath: path.resolve(
+        __dirname,
+        `../../docs/projects/${pkg.challenge.name}`
+      ),
+    }),
+  ],
+});
