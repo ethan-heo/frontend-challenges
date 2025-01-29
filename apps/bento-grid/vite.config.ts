dbc34d401345d4e7273ed039f98ef9c91dec2770
ethan-heo <ethan.hb94@gmail.com>
2025-01-29 23:34:48 +0900

    chore: baseURL 정보를 추가한다

--- apps/bento-grid/vite.config.ts
+++ apps/bento-grid/vite.config.ts
@@ -4,6 +4,7 @@ import moveDistFiles from "vite-plugin-move-dist-files";
 import pkg from "./package.json";
 
 export default defineConfig({
+  base: `/frontend-challenges/projects/${pkg.challenge.name}/`,
   plugins: [
     moveDistFiles({
       distPath: path.resolve(__dirname, "dist"),
