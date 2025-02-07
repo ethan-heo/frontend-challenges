721c784db53b390bf8a3eabaa9698b1029717ba5
ethan-heo <ethan.hb94@gmail.com>
2025-02-07 20:30:18 +0900

    fix: 제품 수량이 0보다 작아지는 이슈 해결

--- apps/product-list-with-cart/src/hooks/useProductStore.hook.ts
+++ apps/product-list-with-cart/src/hooks/useProductStore.hook.ts
@@ -55,7 +55,7 @@ export const productItemStore = {
       productItem.id === id
         ? {
             ...productItem,
-            quantity,
+            quantity: quantity > 0 ? quantity : 1,
           }
         : productItem,
     );
