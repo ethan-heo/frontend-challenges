dc241cee291cd1ab066c500a4d3a2646f5fae1ee
ethan-heo <ethan.hb94@gmail.com>
2025-02-18 21:57:22 +0900

    chore: CI 액션 job 네이밍 수정

---
+++ apps/rock-paper-scissors-game/src/App.tablet.styles.ts
@@ -0,0 +1,7 @@
+import { css } from "@emotion/css";
+
+const tablet = css`
+  padding: 28px;
+`;
+
+export default tablet;
---
+++ apps/rock-paper-scissors-game/src/components/Header/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+  import useMediaQuery from "use-media-query/svelte";
+  import common from "./Header.common";
+  import desktop from "./Header.desktop";
+
+  const { style } = useMediaQuery({
+    desktop,
+  });
+</script>
+
+<div class={[common, style]}>
+  <div>A</div>
+  <div>B</div>
+</div>
