3342190ae099ce0a3f04c81d2c8379781d3282b9
ethan-heo <ethan.hb94@gmail.com>
2025-02-21 22:30:16 +0900

    fix: 불필요한 모듈 제거

--- apps/rock-paper-scissors-game/src/components/Header/UseCase.svelte
+++ apps/rock-paper-scissors-game/src/components/Header/UseCase.svelte
@@ -1,12 +1,10 @@
 <script>
-  import useMediaQuery from "use-media-query/svelte";
   import common from "./UseCase.common.styles";
 
-  const style = useMediaQuery({});
   const CASES = ["ROCK", "PAPER", "SCISSORS", "LIZARD", "SPOCK"];
 </script>
 
-<ul class={[common, style]}>
+<ul class={common}>
   {#each CASES as CASE}
     <li>{CASE}</li>
   {/each}
